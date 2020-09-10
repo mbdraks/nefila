31853698508ce67878c2a5cc74441fd263c12149
mbdraks <2147047+mbdraks@users.noreply.github.com>
2020-09-10 17:01:52 -0300

    bump version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="nefila", 
     packages=find_packages(),
-    version = '0.0.3',
+    version = '0.0.4',
     license='GPL-3',
     description = 'Nefila is an elegant and simple Fortinet Security Fabric library for Python.',
     author = 'Michel Barbosa',
-    author_email = 'nothing@nodomain.com',
-    url = 'https://github.com/barbosm/nefila',
-    download_url = 'https://github.com/barbosm/nefila/archive/v0.0.3.tar.gz',
+    author_email = 'nefila@deadpackets.io',
+    url = 'https://github.com/mbdraks/nefila',
+    download_url = 'https://github.com/mbdraks/nefila/archive/v0.0.4.tar.gz',
     keywords = ['fortinet', 'fortigate', 'security fabric'],
     install_requires=[
         'requests', 'requests[security]'
@@ -17,7 +17,6 @@ setup(
     classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     'Programming Language :: Python :: 3.7',
     ],
